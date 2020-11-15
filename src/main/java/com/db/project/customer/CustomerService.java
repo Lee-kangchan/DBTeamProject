13248182e32be40dd351f22fe3a2a67c6a791c0f
chan <lsc4237@gmail.com>
2020-11-15 23:29:51 +0900

    Service 비즈니스 로직 작성

---
+++ src/main/java/com/db/project/customer/CustomerService.java
@@ -0,0 +1,8 @@
+package com.db.project.customer;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface CustomerService {
+    public List<HashMap<String , Object>> selectCustomer();
+}
---
+++ src/main/java/com/db/project/customer/CustomerServiceImpl.java
@@ -0,0 +1,20 @@
+package com.db.project.customer;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements  CustomerService{
+    @Autowired
+    SqlSession sqlSession;
+
+    @Override
+    public List<HashMap<String, Object>> selectCustomer() {
+        CustomerDAO dao = new CustomerDAO(sqlSession);
+        return dao.selectCustomer();
+    }
+}
