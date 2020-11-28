f62f6551a3c96d9037a9ebd3b7e4273f699a457c
Lee-kangchan <lsc4237@gmail.com>
2020-11-29 01:09:12 +0900

    <<init>> promotion Query DAO 추가

--- src/main/java/com/db/project/promotion/PromotionDAO.java
+++ src/main/java/com/db/project/promotion/PromotionDAO.java
@@ -1,4 +1,28 @@
 package com.db.project.promotion;
 
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class PromotionDAO {
+    PromotionMapper mapper;
+    SqlSession sqlSession;
+
+    public PromotionDAO(SqlSession sqlSession){
+        this.sqlSession = sqlSession;
+        mapper = sqlSession.getMapper(PromotionMapper.class);
+    }
+    public List<HashMap<String, Object>> selectProcessPromotion(HashMap<String,Object> HashMap){
+        return mapper.selectProcessPromotion(HashMap);
+    }
+    public List<HashMap<String, Object>> selectPromotion(HashMap<String, Object> HashMap){
+        return mapper.selectPromotion(HashMap);
+    }
+    public HashMap<String, Object> selectDetailPromotion(HashMap<String, Object> HashMap){
+        return mapper.selectDetailPromotion(HashMap);
+    }
+    public void insertPromotion(HashMap<String, Object> HashMap){
+        mapper.insertPromotion(HashMap);
+    }
 }
--- src/main/java/com/db/project/promotion/PromotionMapper.java
+++ src/main/java/com/db/project/promotion/PromotionMapper.java
@@ -1,4 +1,12 @@
 package com.db.project.promotion;
 
+import java.util.List;
+import java.util.HashMap;
+
 public interface PromotionMapper {
+
+    public List<HashMap<String, Object>> selectProcessPromotion(HashMap<String,Object> HashMap);
+    public List<HashMap<String, Object>> selectPromotion(HashMap<String, Object> HashMap);
+    public HashMap<String, Object> selectDetailPromotion(HashMap<String, Object> HashMap);
+    public void insertPromotion(HashMap<String, Object> HashMap);
 }
