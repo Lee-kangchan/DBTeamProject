0b375af2addcc178fdaec6cae67d4822b3cea3dd
Lee-kangchan <lsc4237@gmail.com>
2020-11-29 01:08:31 +0900

    <<init>> Customer Query DAO 추가

--- src/main/java/com/db/project/customer/CustomerDAO.java
+++ src/main/java/com/db/project/customer/CustomerDAO.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.session.SqlSession;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CustomerDAO {
 
@@ -17,5 +18,41 @@ public class CustomerDAO {
 
     public List<HashMap<String , Object>> selectCustomer(){
         return mapper.selectCustomer();
+    }
+    public void insertCustomer(HashMap<String, Object> HashMap){
+        mapper.insertCustomer(HashMap);
+    }
+    public Integer selectCustomer(HashMap<String, Object> HashMap){
+        return mapper.selectCustomer(HashMap);
+    }
+    public void insertPreference(HashMap<String, Object> HashMap){
+        mapper.insertPreference(HashMap);
+    }
+    public Integer selectCustomerSeq(HashMap<String, Object> HashMap){
+        return mapper.selectCustomerSeq(HashMap);
+    }
+    public HashMap<String, Object> login(HashMap<String, Object> HashMap){
+        return mapper.login(HashMap);
+    }
+    public void updateRentalMembership(HashMap<String, Object> HashMap){
+        mapper.updateRentalMembership(HashMap);
+    }
+    public void updateBurrowMembership(HashMap<String, Object> HashMap){
+        mapper.updateBurrowMembership(HashMap);
+    }
+    public void insertBestRental(HashMap<String, Object> HashMap){
+        mapper.insertBestRental(HashMap);
+    }
+    public void insertBestBorrow(HashMap<String, Object> HashMap){
+        mapper.insertBestBorrow(HashMap);
+    }
+    public void deleteBestRental(){
+        mapper.deleteBestRental();
+    }
+    public void deleteBestBorrow(){
+        mapper.deleteBestBorrow();
+    }
+    public Integer customerSales(HashMap<String,Object> HashMap){
+        return mapper.customerSales(HashMap);
     }
 }
--- src/main/java/com/db/project/customer/CustomerMapper.java
+++ src/main/java/com/db/project/customer/CustomerMapper.java
@@ -5,4 +5,17 @@ import java.util.List;
 
 public interface CustomerMapper {
     public List<HashMap<String, Object>> selectCustomer();
+    public void insertCustomer(HashMap<String, Object> HashMap);
+    public Integer selectCustomer(HashMap<String, Object> HashMap);
+    public void insertPreference(HashMap<String, Object> HashMap);
+    public Integer selectCustomerSeq(HashMap<String, Object> HashMap);
+    public HashMap<String, Object> login(HashMap<String, Object> HashMap);
+    public void updateRentalMembership(HashMap<String, Object> HashMap);
+    public void updateBurrowMembership(HashMap<String, Object> HashMap);
+    public void insertBestRental(HashMap<String, Object> HashMap);
+    public void insertBestBorrow(HashMap<String, Object> HashMap);
+    public void deleteBestRental();
+    public void deleteBestBorrow();
+    public Integer customerSales(HashMap<String,Object> HashMap);
+
 }
