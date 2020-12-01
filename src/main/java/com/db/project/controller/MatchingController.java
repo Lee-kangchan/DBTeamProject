48b763f5c2540c051f83d62ae00faef77f99b76c
Lee-kangchan <lsc4237@gmail.com>
2020-12-01 15:34:22 +0900

    <<Update>> Matching Controller 작성

--- src/main/java/com/db/project/controller/MatchingController.java
+++ src/main/java/com/db/project/controller/MatchingController.java
@@ -3,13 +3,55 @@ package com.db.project.controller;
 import com.db.project.matching.MatchingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 
 @Controller
 public class MatchingController {
 
     @Autowired
     MatchingService matchingService;
-
-
+    @PostMapping("/insert/matching/{book_seq}")
+    public String insertMatching(@PathVariable Integer book_seq, HttpSession session){
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("book_seq",book_seq);
+        map.put("customer_seq", session.getAttribute("customer_seq"));
+        matchingService.insertMatching(map);
+        return "redirect:/home";
+    }
+    @GetMapping("/select/matching/rental")
+    public String selectRentalMatching(Model model,HttpSession session){
+        HashMap<String , Object> map = new HashMap<>();
+        map.put("customer_seq", session.getAttribute("customer_seq"));
+        model.addAttribute("matching",matchingService.selectRentalMatching(map));
+        return "matching";
+    }
+    @GetMapping("/select/matching/borrow")
+    public String selectBorrowMatching(Model model, HttpSession session){
+        HashMap<String , Object> map = new HashMap<>();
+        map.put("customer_seq", session.getAttribute("customer_seq"));
+        model.addAttribute("matching",matchingService.selectRentalMatching(map));
+        return "matching";
+    }
+    @PostMapping("/update/rental/check")
+    public String  updateRentalCheck(Model model, HttpSession session){
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("customer_seq",session.getAttribute("customer_seq"));
+        matchingService.updateRentalMatching(hashMap);
+        return "redirect:/select/matching/rental";
+    }
+    @PostMapping("/update/borrow/chcek")
+    public String  updateBorrowCheck(Model model, HttpSession session){
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("customer_seq",session.getAttribute("customer_seq"));
+        matchingService.updateBorrowMatching(hashMap);
+        return "redirect:/select/matching/borrow";
+    }
 
 }
